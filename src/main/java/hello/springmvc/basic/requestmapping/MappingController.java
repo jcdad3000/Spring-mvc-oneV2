f63b659740cab488776f5eeadeeed5d3ed4b6864
jcdad3000 <31876648+jcdad3000@users.noreply.github.com>
2022-10-03 20:23:36 +0900

    요청 매핑
    - 여러가지 요청 매핑하는 방식 적용

--- src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -2,16 +2,20 @@ package hello.springmvc.basic.requestmapping;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class MappingController {
     private Logger log = LoggerFactory.getLogger(getClass());
 
-    @RequestMapping("/hello-basic")
+    @RequestMapping(value = "/hello-basic",method = RequestMethod.GET)
     public String helloBasic(){
         log.info("helloBasic");
         return "OK";
+    }
+    @GetMapping("/mapping/{userId}")
+    public String mappingPath(@PathVariable("userId") String data){
+        log.info("mappingPath userId={}",data);
+        return "ok";
     }
 }
